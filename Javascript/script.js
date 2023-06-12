95b5bd389a6788510b176f10257a746e830a391e
Eliazith Zeledon <Eliazithzeledon27@gmail.com>
2023-06-11 22:41:30 -0600

    Mejora en la estructura HTML dinamica + btn saludo

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,13 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
+const saludarButton = document.getElementById('saludarButton');
+const contenido = document.querySelector('.contenido');
+
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');    
 });
+
+function showButton() {
+    saludarButton.classList.remove('hidden');
+}
