ba7a1d390ea14d4c4d74ddaf522e6eb9e190ab79
Eliazith Zeledon <Eliazithzeledon27@gmail.com>
2023-06-12 01:39:50 -0600

    + Mensaje de saludo + ver y remover mensaje

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,6 +2,8 @@ const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.menu');
 const saludarButton = document.getElementById('saludarButton');
 const contenido = document.querySelector('.contenido');
+const saludoMensaje = document.getElementById('saludoMensaje');
+
 
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
@@ -10,4 +12,15 @@ menuToggle.addEventListener('click', () => {
 
 function showButton() {
     saludarButton.classList.remove('hidden');
+}
+
+function showSaludo() {
+    saludoMensaje.classList.remove("oculto");
+    saludoMensaje.classList.add("show");
+}
+
+function ocultarMensaje() {
+    var saludoMensaje = document.getElementById("saludoMensaje");
+    saludoMensaje.classList.remove("show");
+    saludoMensaje.classList.add("oculto");
 }
